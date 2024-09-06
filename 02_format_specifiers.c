8965ddb72cf22e5bdc7acb89a557e3460a58761b
pvuppu2 <pvuppu2@student.gitam.edu>
2024-09-06 11:04:00 +0530

    Create 02_format_specifiers.c

---
+++ 02_format_specifiers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 15;
+    printf("Decimal representation: %d \nOctal representation: %o\nHexademical representation: %x", a, a, a);
+
+    return 0;
+}
+
+/* Decimal representation: 15 
+Octal representation: 17
+Hexademical representation: f */

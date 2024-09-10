32a0fc82a28f2e5f8b4ef44c36da77ed97357224
pvuppu2 <pvuppu2@student.gitam.edu>
2024-09-10 08:35:47 +0530

    Create 06_whilefibonocci.c

---
+++ 06_whilefibonocci.c
@@ -0,0 +1,23 @@
+# include <stdio.h>
+int main() {
+  
+  int number_of_terms;
+  int fibonacci_1 = 0, fibonacci_2 = 1;
+
+  int fibonacci_3 = fibonacci_1 + fibonacci_2;
+
+    printf("Enter the number of terms (greater than 2): ");
+    scanf("%d", &number_of_terms);
+
+    printf("Fibonacci Series: \n%d \n%d", fibonacci_1, fibonacci_2);
+
+int i = 3;
+while(i <= number_of_terms) {
+    printf("\n%d", fibonacci_3);
+    fibonacci_1 = fibonacci_2;
+    fibonacci_2 = fibonacci_3;
+    fibonacci_3 = fibonacci_1 + fibonacci_2;
+    i++;
+}
+return 0;
+}
